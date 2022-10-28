87e14fc120b826d8a0090333c9c462bea94b0c3f
Adeniran John <adeniranjohn2016@gmail.com>
2022-10-28 15:13:31 -0700

    task 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalize word
+ * @s: string
+ * Return: pointer to char
+ */
+
+char *cap_string(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[0] >= 97 && s[0] <= 122)
+		{
+			s[0] = s[0] - 32;
+		}
+
+		if (s[i] == ' ' || s[i] == '\t' || s[i] == '\n'
+			|| s[i] == ',' || s[i] == ';' || s[i] == '.'
+			|| s[i] == '.' || s[i] == '!' || s[i] == '?'
+			|| s[i] == '"' || s[i] == '(' || s[i] == ')'
+			|| s[i] == '{' || s[i] == '}')
+		{
+			if (s[i + 1] >= 97 && s[i + 1] <= 122)
+			{
+				s[i + 1] = s[i + 1] - 32;
+			}
+		}
+		i++;
+	}
+	return (s);
+}		
