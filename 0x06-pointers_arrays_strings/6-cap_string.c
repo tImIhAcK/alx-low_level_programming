c72e819752683ed61771b68540dbb9a7b820fcec
Adeniran John <adeniranjohn2016@gmail.com>
2022-10-28 15:29:07 -0700

    task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -17,7 +17,7 @@ char *cap_string(char *str)
 
 		if (str[i - 1] == ' ' || str[i - 1] == '\t' || str[i - 1] == '\n'
 			|| str[i - 1] == ',' || str[i - 1] == ';' || str[i - 1] == '.'
-			|| str[i - 1] == '.' || str[i -1 ] == '!' || str[i - 1] == '?'
+			|| str[i - 1] == '.' || str[i - 1] == '!' || str[i - 1] == '?'
 			|| str[i - 1] == '"' || str[i - 1] == '(' || str[i - 1] == ')'
 			|| str[i - 1] == '{' || str[i - 1] == '}' || i == 0)
 
@@ -26,4 +26,4 @@ char *cap_string(char *str)
 		i++;
 	}
 	return (str);
-}	
+}
