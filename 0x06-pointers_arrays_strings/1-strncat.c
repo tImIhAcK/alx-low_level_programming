44b5c78bb0a174ad7a1780ffdfc5781fa1f687ca
Adeniran John <adeniranjohn2016@gmail.com>
2022-10-27 22:22:38 -0700

    task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,6 +11,21 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int lengh, j;
+	int i, j;
 
-	length = 0;
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+
+	dest[i] = '\0';
+	return (dest);
+}
